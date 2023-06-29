f0fa9344b770a88549192bf74b82c359be387ca2
mconf248 <mark@conf.cx>
2023-06-28 22:37:30 -0400

    Updated

--- current_weather.py
+++ current_weather.py
@@ -1,18 +1,16 @@
-#Simple script to obtain weather information from WeatherStack API
-
 import main_config
 import requests
 from prettytable import PrettyTable
 
-api_request = requests.get('http://api.weatherstack.com/current?access_key='+ main_config.WEATHERSTACK_API +'&query='+ main_config.WEATHER_ZIPCODE +'')
+api_request = requests.get('http://api.weatherstack.com/current?access_key=' + main_config.WEATHERSTACK_API + main_config.WEATHER_ZIPCODE + main_config.WEATHER_UNITS)
 api_data = api_request.json()
 
 myTable = PrettyTable(["City", "State", "Conditions", "Feels Like (C)", "Temperature (C)"])
 myTable.align = "c"
 
 weather_descriptions = []
-for element in api_data["current"]["weather_descriptions"]:
-    weather_descriptions.append(element)
+for weather_condition in api_data["current"]["weather_descriptions"]:
+    weather_descriptions.append(weather_condition)
 
-myTable.add_row([api_data["location"]["name"], api_data["location"]["region"], ", ".join(weather_descriptions) ,api_data["current"]["feelslike"],api_data["current"]["temperature"] ])
+myTable.add_row([api_data["location"]["name"], api_data["location"]["region"], ", ".join(weather_descriptions), api_data["current"]["feelslike"], api_data["current"]["temperature"]])
 print(myTable)
