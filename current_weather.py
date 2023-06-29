10fb8a7a25ed0e9e8ce05c980fa9b7b963b6c38e
mconf248 <mark@conf.cx>
2023-06-28 22:16:45 -0400

    Updated tables

--- current_weather.py
+++ current_weather.py
@@ -2,8 +2,17 @@
 
 import main_config
 import requests
-import table_config
+from prettytable import PrettyTable
 
-r = requests.get('http://api.weatherstack.com/current?access_key='+ main_config.WEATHERSTACK_API +'&query='+ main_config.WEATHER_ZIPCODE +'')
-data = r.json()
-print(data)
+api_request = requests.get('http://api.weatherstack.com/current?access_key='+ main_config.WEATHERSTACK_API +'&query='+ main_config.WEATHER_ZIPCODE +'')
+api_data = api_request.json()
+
+myTable = PrettyTable(["City", "State", "Conditions", "Feels Like (C)", "Temperature (C)"])
+myTable.align = "c"
+
+weather_descriptions = []
+for element in api_data["current"]["weather_descriptions"]:
+    weather_descriptions.append(element)
+
+myTable.add_row([api_data["location"]["name"], api_data["location"]["region"], ", ".join(weather_descriptions) ,api_data["current"]["feelslike"],api_data["current"]["temperature"] ])
+print(myTable)
