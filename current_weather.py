2e47141bbf28926914d86857da8cf69ef89b8f9e
mconf248 <127979794+mconf248@users.noreply.github.com>
2023-07-01 00:42:57 -0400

    Commit

--- current_weather.py
+++ current_weather.py
@@ -6,7 +6,7 @@ api_request = requests.get('http://api.weatherstack.com/current?access_key=' + m
 if api_request.status_code == 200:
     api_data = api_request.json()
 
-myTable = PrettyTable(["City", "State", "Conditions", "Feels Like (F)", "Precip", "UV Index"])
+myTable = PrettyTable(["City", "State", "Conditions", "Feels Like (F)", "Temeperature (F)", "Humidity", "Precip", "UV Index"])
 myTable.align = "c"
 
 weather_descriptions = []
@@ -16,7 +16,9 @@ for weather_condition in api_data["current"]["weather_descriptions"]:
 myTable.add_row([api_data["location"]["name"], 
                  api_data["location"]["region"], 
                  "".join(weather_descriptions), 
-                 api_data["current"]["feelslike"], 
+                 api_data["current"]["feelslike"],
+                 api_data["current"]["temperature"],
+                 api_data["current"]["humidity"], 
                  api_data["current"]["precip"],
                  api_data["current"]["uv_index"],])
 
